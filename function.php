99fafb88100d83ea2f87dd5196ce4d0b6869d217
Priya dharshini k <77014476+Priyakumaran08@users.noreply.github.com>
2025-05-18 11:26:11 +0530

    Register Custom Endpoint

--- function.php
+++ function.php
@@ -1,3 +1,4 @@
+//Create Custom Post Type - Testimonials
 function custom_testimonial_type(){
 	register_post_type('testimonials', array(
 		'labels'=> array(
@@ -10,3 +11,16 @@ function custom_testimonial_type(){
 	));
 }
 add_action('init', 'custom_testimonial_type');
+
+//Register Custom Endpoints
+add_action('rest_api_init', 'custom_testimonial_route');
+function custom_testimonial_route(){
+	register_rest_route(
+		'custom/v1',
+		'/testimonials',
+		array(
+		'methods' => 'GET',
+		'callback' => 'get_testimonials',
+		)
+	);
+}
